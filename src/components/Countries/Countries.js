45b9231aa398e1f06df57913c263c5e8e6fce03b
Sadman Sakib <sadmansakib123450@gmail.com>
2023-03-23 14:41:01 +0100

    grid style and country flags added from the api

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../SingleCountry/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountry] = useState([])
@@ -9,22 +10,26 @@ const Countries = () => {
         .then(data=> setCountry(data))
     }, []);
     return (
-        <div>
+        <div >
             <h1>Hello World</h1>
+            <div className='countries-container'>
             {
                 countries.map(country=> <Country
                 //aikhane issa kore sadman nam disi. bam pasher nam jeta dibo props er por
                 // sei nam dhorei call korte hbe. exm: Country.js file a amra props.sadman
                 // likhesi
+                
                 sadman ={country.name.common}
                 timezone = {country.timezones}
 
                 // uporer moto country er ekta ekta data Country.js a na pathiye amra
                 // direct country ta kei Country.js a patahi dite pari then oikhane theke data show kora jabe
                 country ={country}
-                
+                // eta warning shoranor jonno ekta key diyesi
+                key={country.cca3}
                 ></Country>)
             }
+            </div>
             
         </div>
     );
--- src/components/SingleCountry/Country.js
+++ src/components/SingleCountry/Country.js
@@ -2,12 +2,27 @@ import React from 'react';
 import  './Country.css'
 
 const Country = (props) => {
+    // destructure koreo data pawa jai.niche destructure kore dekhano holo:
+    
+    const {region, flags} = props.country;
     return (
         <div className='singleCountry'>
+          
+            {/* ai duita distructure bade kora */}
             <h1>Country Name: {props.sadman}</h1>
+
+            {/* img er line tao destructure er maddhome kora */}
+            <img src={flags.png} alt={flags.alt} />
+
             <p>TimeZone: {props.timezone}</p>
+            
+            
             {/* Country pura pathanor por Country theke data pawar niwom niche dea holo */}
             <p>Area: {props.country.area}</p>
+            
+            
+            {/* Destructure korar por data pawar niwom niche: */}
+            <p>Region: {region}</p>
         </div>
     );
 };
